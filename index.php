3945462548b09970e4bfa5ddcd18b28af226ae55
Vale <Vale@Vale-PC>
2015-06-01 19:16:56 +0300

    - created new files

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+ * @author Vale
+ * @created on Jun 1, 2015
+ * @created at 7:14:11 PM
+ */
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+ * @author Vale
+ * @created on Jun 1, 2015
+ * @created at 7:15:16 PM
+ */
